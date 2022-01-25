be0ba3ad6fdce2160faa6c275eb12ef3d92471fe
ljoonal <ljoonal@ljoonal.xyz>
2022-01-25 02:44:23 +0200

    Update PrivacyShield

--- PrivacyShield/BuildInfo.cs
+++ PrivacyShield/BuildInfo.cs
@@ -11,7 +11,7 @@ namespace PrivacyShield
 {
 	public static class BuildInfo
 	{
-		public const string Version = "0.1.0";
+		public const string Version = "0.2.0";
 
 		public const string Name = "Privacy Shield";
 
--- PrivacyShield/PrivacyShield.cs
+++ PrivacyShield/PrivacyShield.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using HarmonyLib;
 using NeosModLoader;
 using UnityEngine;
@@ -16,25 +17,33 @@ namespace PrivacyShield
 		public override string Link => BuildInfo.Link;
 
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<float> SpoofFPS = new("FpsSpoof", "The FPS to spoof to.", () => 30f);
+		private static readonly ModConfigurationKey<float> SpoofFPS = new("FpsSpoof", "The FPS to spoof to. Set to 0 to disable.", () => 0f);
 		[AutoRegisterConfigKey]
 		private static readonly ModConfigurationKey<string> TimeZoneSpoof = new("TimeZoneSpoof", "The timezone to spoof to.", () => "UTC");
+		[AutoRegisterConfigKey]
+		private static readonly ModConfigurationKey<bool> TimeZoneSpoofEnabled = new("TimeZoneSpoofEnabled", "If the TZ spoof is enabled or not.", () => false);
 
-		private static PrivacyShieldMod Instance;
+
+		private static ModConfiguration Config;
 		//private static readonly System.Random rng = new();
 
 		public override void OnEngineInit()
 		{
-			Instance = this;
-			try
+			Config = GetConfiguration();
+			Config.Save(true);
+			if (Config.GetValue(TimeZoneSpoofEnabled))
 			{
-				var config = Instance.GetConfiguration();
-				Traverse.Create(typeof(TimeZoneInfo)).Field("local").SetValue(TimeZoneInfo.FindSystemTimeZoneById(config.GetValue(TimeZoneSpoof)));
+				try
+				{
+
+					Traverse.Create(typeof(TimeZoneInfo)).Field("local").SetValue(TimeZoneInfo.FindSystemTimeZoneById(Config.GetValue(TimeZoneSpoof)));
+				}
+				catch (Exception ex)
+				{
+					Error(ex);
+				}
 			}
-			catch (Exception ex)
-			{
-				Error(ex);
-			}
+
 			try
 			{
 				Harmony harmony = new(BuildInfo.GUID);
@@ -51,12 +60,62 @@ namespace PrivacyShield
 		[HarmonyPostfix]
 		private static void PatchFPS(ref FrooxEngine.World __instance)
 		{
-			var config = Instance.GetConfiguration();
-			var targetFPS = config.GetValue(SpoofFPS);
-			// Run original getter if spoofing is disabled
+			var targetFPS = Config.GetValue(SpoofFPS);
+			// Short circuit with monkee values.
+			// This is meant for privacy, not for "oh look number go brr"
 			if (targetFPS <= 10 || targetFPS >= 144) return;
 			//double random = rng.NextDouble() * config.GetValue(VarianceFPS);
 			__instance.LocalUser.FPS = targetFPS;
+		}
+
+
+		[HarmonyPatch(typeof(FrooxEngine.AssetManager), nameof(FrooxEngine.AssetManager.RequestGather))]
+		[HarmonyPrefix]
+		private static bool PatchRequester(
+			FrooxEngine.AssetManager __instance,
+			FrooxEngine.AssetGatherer ___assetGatherer,
+			ref ValueTask<string> __result,
+			Uri __0,
+			FrooxEngine.Priority __1,
+			CloudX.Shared.NeosDB_Endpoint? __2
+		)
+		{
+			__result = HandleRequest(__instance, ___assetGatherer, __0, __1, __2);
+			return false;
+		}
+
+		private static async ValueTask<string> HandleRequest(
+			FrooxEngine.AssetManager assetManager,
+				FrooxEngine.AssetGatherer assetGatherer,
+				Uri uri,
+				FrooxEngine.Priority priority,
+				CloudX.Shared.NeosDB_Endpoint? endpointOverwrite
+		)
+		{
+			bool has_perms;
+			if (uri.Scheme == "neosdb" || uri.Scheme == "local") has_perms = true;
+			else
+			{
+				Msg("Asking permissions for", uri);
+				has_perms = await AskForPermission(assetManager.Engine, uri, "PrivacyShield generic request");
+			}
+			if (has_perms)
+			{
+				return await assetGatherer.Gather(uri, priority, endpointOverwrite);
+			}
+			else
+			{
+				throw new Exception("No permissions to load asset");
+			}
+		}
+
+
+		private static async Task<bool> AskForPermission(FrooxEngine.Engine engine, Uri target, String accessReason)
+		{
+			FrooxEngine.HostAccessPermission perms = await
+				engine.Security.RequestAccessPermission(target.Host, target.Port,
+				accessReason);
+			return perms == FrooxEngine.HostAccessPermission.Allowed;
 		}
 	}
 }
