46a57241326eae307a62d61f85bb9f1ae859446b
ljoonal <ljoonal@ljoonal.xyz>
2022-04-03 18:42:08 +0300

    Add hash generation for future releases

--- gitea-publish.sh
+++ gitea-publish.sh
@@ -4,10 +4,22 @@ set -e
 # Get current version
 VERSION=`date --iso-8601`
 
-FILES_ARG=`find bin/Release/ -type f | awk 1 ORS=' -a '`
+cd bin/Release
+
+# Create hashes of all the files (except the hash files themselves)
+rm -f sha512sums.txt blake3sums.txt
+SHA512SUMS=`sha512sum *`
+BLAKE3SUMS=`b3sum *`
+echo "$SHA512SUMS" > sha512sums.txt
+echo "$BLAKE3SUMS" > blake3sums.txt
+
+# Create args to be passed to tea for uploading all the files
+FILES_ARG=`find ./ -type f | awk 1 ORS=' -a '`
 FILES_ARG="-a ${FILES_ARG%????}"
 
 # Create a gitea release draft
 tea release create --draft --target main \
   --tag v$VERSION --title v$VERSION \
   $FILES_ARG
+
+cd ../..
